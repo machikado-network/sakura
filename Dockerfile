547dec415426030aeb4f41677529e09d86121fb8
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2022-08-14 07:00:24 +0900

    test for sakura tinc setup

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM rust:1.62.1 as builder
+#
+# ビルド用ステージ
+#
+# See: https://hub.docker.com/_/rust
+#
+FROM rust:1.63-slim-buster as build_env
 
 RUN apt-get update -y && apt-get upgrade -y
 
@@ -13,4 +18,18 @@ RUN --mount=type=cache,target=/usr/local/cargo/registry \
     --mount=type=cache,target=/sakura/target \
     cargo install --path .
 
-CMD ["/usr/local/cargo/bin/sakura", "tinc", "setup", "syamimomo", "10.50.2.1"]
+#
+# 実行用ステージ
+#
+FROM debian:buster-slim
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get -y update && \
+    apt-get -y install systemd && \
+    apt-get clean
+
+COPY --from=build_env /usr/local/cargo/bin/sakura /usr/local/sbin/sakura
+
+STOPSIGNAL SIGRTMIN+3
+CMD [ "/sbin/init" ]
---
+++ test_setup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#
+# sakura tinc setup のテスト
+#
+# RUN:
+#   ./test_setup.sh
+#
+# DEBUG:
+#   sh -x ./test_setup.sh
+#
+
+tinc_node_name="syamimomo"
+docker_name="sakura_syamimomo_test"
+
+echo_green() {
+  printf '\e[32m%s\e[m\n' "${1}"
+}
+
+echo_red() {
+  printf '\e[31m%s\e[m\n' "${1}"
+}
+
+tmp="$(mktemp -d)"
+echo "tmp created at: ${tmp}" >&2
+
+docker build -t "${docker_name}" . &&
+docker run -d --privileged -v "${tmp}":/etc/tinc --name="${docker_name}" "${docker_name}" &&
+sleep 1 &&
+docker exec "${docker_name}" sakura tinc setup "${tinc_node_name}" 10.50.255.1 &&
+docker stop "${docker_name}" &&
+docker rm "${docker_name}"
+
+if ! test -f "${tmp}"/mchkd/hosts/"${tinc_node_name}"; then
+  {
+    echo_red "File not found: ${tmp}/mchkd/hosts/${tinc_node_name}"
+    echo_red "TEST FAILED"
+  } >&2
+  exit 1
+fi
+
+if ! grep -- '-----BEGIN RSA PUBLIC KEY-----' "${tmp}"/mchkd/hosts/"${tinc_node_name}" > /dev/null; then
+  {
+    echo_red "RSA pubkey not found in: ${tmp}/mchkd/hosts/${tinc_node_name}"
+    echo_red "TEST FAILED"
+  } >&2
+  exit 1
+fi
+
+rm -rf "${tmp}"
+echo_green "TEST SUCCESS!" >&2
