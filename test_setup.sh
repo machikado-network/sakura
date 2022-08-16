506763147d1c4052c60d1726b4f7689c57863685
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2022-08-16 22:05:40 +0900

    Add test for changing beidge name

--- test_setup.sh
+++ test_setup.sh
@@ -10,8 +10,11 @@
 #   sh -x ./test_setup.sh
 #
 
+set -eu
+
 tinc_node_name="syamimomo"
 docker_name="sakura_syamimomo_test"
+
 
 echo_green() {
   printf '\e[32m%s\e[m\n' "${1}"
@@ -21,30 +24,97 @@ echo_red() {
   printf '\e[31m%s\e[m\n' "${1}"
 }
 
-tmp="$(mktemp -d)"
-echo "tmp created at: ${tmp}" >&2
+# テストを失敗させるときに呼ぶシェル関数
+testfail() {
+  {
+    echo_red "${1}"
+    echo_red "TEST FAILED"
+  } >&2
+  exit 1
+}
 
-docker build --no-cache -t "${docker_name}" . &&
-docker run --rm -d --privileged -v "${tmp}":/etc/tinc --name="${docker_name}" "${docker_name}" &&
-sleep 1 &&
-docker exec "${docker_name}" sakura tinc setup "${tinc_node_name}" 10.50.255.1 &&
+mktmp() {
+  tmp="$(mktemp -d)" || testfail "テンポラリディレクトリの作成に失敗しました"
+  echo "tmp created at: ${tmp}" >&2
+}
+
+rmtmp() {
+  rm -rf "${tmp}"
+}
+
+rmtmpc() {
+  rm -rf "${tmp:?}"/*
+}
+
+onexit() {
+  {
+    set +e
+    docker stop "${docker_name}"
+    rmtmp
+  } > /dev/null 2>&1
+}
+
+trap onexit EXIT
+mktmp
+
+#
+# テスト用イメージのビルド
+#
+docker build --no-cache -t "${docker_name}" . ||
+  testfail "docker build failed"
+
+
+#
+# ブリッジインターフェースの重複回避のテスト
+#
+rmtmpc
+
+# br0 が存在する状態で sakura tinc setup が失敗するかチェック
+! {
+  docker run --rm -d --privileged --name="${docker_name}" "${docker_name}" &&
+    sleep 1 &&
+    docker exec "${docker_name}" ip link add br0 type bridge &&
+    docker exec "${docker_name}" sakura tinc setup "${tinc_node_name}" 10.50.255.1
+} || {
+  docker stop "${docker_name}"
+  testfail "sakura tinc setup の実行は失敗するはずですが, 成功しました"
+}
 docker stop "${docker_name}"
 
-if ! test -f "${tmp}"/mchkd/hosts/"${tinc_node_name}"; then
-  {
-    echo_red "File not found: ${tmp}/mchkd/hosts/${tinc_node_name}"
-    echo_red "TEST FAILED"
-  } >&2
-  exit 1
-fi
+# br1 を指定して sakura tinc setup が成功するかチェック
+{
+  docker run --rm -d --privileged -v "${tmp}":/etc/tinc --name="${docker_name}" "${docker_name}" &&
+    sleep 1 &&
+    docker exec "${docker_name}" ip link add br0 type bridge &&
+    docker exec "${docker_name}" sakura tinc setup -i br1 "${tinc_node_name}" 10.50.255.1 &&
+    docker stop "${docker_name}"
+} || testfail "sakura tinc setup の実行に失敗しました"
 
-if ! grep -- '-----BEGIN RSA PUBLIC KEY-----' "${tmp}"/mchkd/hosts/"${tinc_node_name}" > /dev/null; then
-  {
-    echo_red "RSA pubkey not found in: ${tmp}/mchkd/hosts/${tinc_node_name}"
-    echo_red "TEST FAILED"
-  } >&2
-  exit 1
-fi
+# br1 でtinc-upファイルが作成されているかチェック
+grep 'ip link add br1 type bridge' < "${tmp}"/mchkd/tinc-up > /dev/null ||
+  testfail "L${LINENO:-?}: /etc/tinc/mchkd/tinc-up が正しく作成されていません"
 
-rm -rf "${tmp}"
+
+#
+# RSA鍵の配置のテスト
+#
+rmtmpc
+
+# コンテナを起動 -> sakura tinc setup を実行 -> コンテナを停止
+{
+  docker run --rm -d --privileged -v "${tmp}":/etc/tinc --name="${docker_name}" "${docker_name}" &&
+    sleep 1 &&
+    docker exec "${docker_name}" sakura tinc setup "${tinc_node_name}" 10.50.255.1 &&
+    docker stop "${docker_name}"
+} || testfail "sakura tinc setup の実行に失敗しました"
+
+# 自身のhost定義が作成されたかをチェック
+test -f "${tmp}"/mchkd/hosts/"${tinc_node_name}" ||
+  testfail "File not found: ${tmp}/mchkd/hosts/${tinc_node_name}"
+
+# 自身のhost定義にRSA公開鍵が存在するかどうかをチェック
+grep -- '-----BEGIN RSA PUBLIC KEY-----' "${tmp}"/mchkd/hosts/"${tinc_node_name}" > /dev/null ||
+  testfail "RSA pubkey not found in: ${tmp}/mchkd/hosts/${tinc_node_name}"
+
+
 echo_green "TEST SUCCESS!" >&2
